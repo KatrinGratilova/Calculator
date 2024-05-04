96aacfd3c72b4dc2c93d73d2151dec2351d89045
KatrinGratilova <gr.katrin.05@gmail.com>
2024-05-04 15:09:05 +0300

    completed calculator

--- src/main/java/org/katrin/Calculator.java
+++ src/main/java/org/katrin/Calculator.java
@@ -1,7 +1,39 @@
 package org.katrin;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 3){
+            System.out.println("Enter 3 numbers: the first operand, operator, the second operand!");
+            return;
+        }
+        double firstOper = Double.parseDouble(args[0]);
+        String operator = args[1];
+        double secondOper = Double.parseDouble(args[2]);
+        double result;
+
+        switch (operator) {
+            case "+":
+                result = firstOper + secondOper;
+                break;
+            case "-":
+                result = firstOper - secondOper;
+                break;
+            case "x":
+                result = firstOper * secondOper;
+                break;
+            case "/":
+                if (secondOper == 0) {
+                    System.out.println("Cannot divide by zero!");
+                    return;
+                }
+                result = firstOper / secondOper;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.print(firstOper + " "  + operator + " " + secondOper + " = " + result);
+
     }
 }
